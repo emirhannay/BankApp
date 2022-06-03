534ddf37a7efd7d93dd1646d7a2c9c1c973b301f
Emirhan <dev.emirhanay@gmail.com>
2022-06-03 17:27:19 +0300

    test

--- src/main/java/com/example/bankapp/config/SecurityConfig.java
+++ src/main/java/com/example/bankapp/config/SecurityConfig.java
@@ -48,7 +48,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
+        http.requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .requiresSecure().and().cors()
                 .and()
                 .csrf()
                 .disable()
