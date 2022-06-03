b79aca9944cea2f48db8999519f658a9a19d2555
Emirhan <dev.emirhanay@gmail.com>
2022-06-03 23:48:01 +0300

    Constraint violation and Data Integrity Exception Handlded

--- src/main/java/com/example/bankapp/controller/BaseControllerAdvice.java
+++ src/main/java/com/example/bankapp/controller/BaseControllerAdvice.java
@@ -1,11 +1,14 @@
 package com.example.bankapp.controller;
 
 import com.example.bankapp.exception.BaseException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class BaseControllerAdvice {
@@ -19,8 +22,16 @@ public class BaseControllerAdvice {
     public ResponseEntity<?> onBaseExceptionHandled(IllegalArgumentException illegalArgumentException) {
         return ResponseEntity.badRequest().body(new ApiError(illegalArgumentException.getMessage()));
     }
-
-
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> onBaseExceptionHandled(DataIntegrityViolationException illegalArgumentException) {
+        return ResponseEntity.badRequest().body(new ApiError(illegalArgumentException.getMessage()));
+    }
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> onBaseExceptionHandled(ConstraintViolationException constraintViolationException) {
+        return ResponseEntity.badRequest().body(new ApiError(constraintViolationException.getMessage()));
+    }
     public static record ApiError(String errorMessage) {
     }
 
