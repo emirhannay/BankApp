753a8fc32d55d9f07e254cd3be4dc8cc0508d92a
Emirhan <dev.emirhanay@gmail.com>
2022-06-04 14:47:28 +0300

    test

--- src/main/java/com/example/bankapp/security/UserDetailService.java
+++ src/main/java/com/example/bankapp/security/UserDetailService.java
@@ -2,6 +2,7 @@ package com.example.bankapp.security;
 
 
 import com.example.bankapp.entity.User;
+import com.example.bankapp.exception.BusinessServiceOperationException;
 import com.example.bankapp.repository.CustomerRepository;
 import com.example.bankapp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
 public class UserDetailService implements UserDetailsService {
@@ -17,7 +20,10 @@ public class UserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException(email + " not found"));
+        User user = userRepository.findByEmail(email);
+        if(Objects.isNull(user)){
+            throw new BusinessServiceOperationException.UserNotFoundException("User not found with this email -> "+email);
+        }
         return new UserDetail(user.getId(),user.getEmail(), user.getPassword(), user.getUserStatus(),user.getRoles());
     }
 }
